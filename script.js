42680646c962b490ae6578239c4aa2ddb304a748
eaz8483 <eaz8483@rit.edu>
2024-11-12 17:26:09 -0500

    images changes on refresh

--- script.js
+++ script.js
@@ -6,10 +6,11 @@ window.onload = function(){
         'https://images.unsplash.com/photo-1527489377706-5bf97e608852?q=80&w=3118&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
         'https://images.unsplash.com/photo-1466854076813-4aa9ac0fc347?q=80&w=2400&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
         'https://images.unsplash.com/photo-1504288145234-919e7bbc6d19?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-    ]
-};
+    ];
+
 
     var imgElement = document.getElementById('cycle-image');
     var randomIndex = Math.floor(Math.random() * images.length);
-    imgElement.src = images[randomIndex]
-    console.log("image",imgElement.src)
+    imgElement.src = images[randomIndex];
+    console.log("image",imgElement.src);
+};
