a9252226ef3b74b207e485b320075fd3388c79e7
eaz8483 <eaz8483@rit.edu>
2024-11-12 17:14:09 -0500

    js file

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //script. js
 window.onload = function(){
+    console.log("Running")
     var images = [
         'https://images.unsplash.com/photo-1498550744921-75f79806b8a7?q=80&w=2400&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', 
         'https://images.unsplash.com/photo-1527489377706-5bf97e608852?q=80&w=3118&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
@@ -11,3 +12,4 @@ window.onload = function(){
     var imgElement = document.getElementById('cycle-image');
     var randomIndex = Math.floor(Math.random() * images.length);
     imgElement.src = images[randomIndex]
+    console.log("image",imgElement.src)
